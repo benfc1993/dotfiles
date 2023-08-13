19755c9c8b0b388efcd3f2e100142debfee70c0f
Ben_Feldberg_Collins <b.feldberg.collins@accenture.com>
2023-08-13 23:26:22 +0100

    add lazygit

--- nvim/plugins.lua
+++ nvim/plugins.lua
@@ -18,6 +18,14 @@ local plugins = {
       },
     },
   },
+  {
+    "kdheepak/lazygit.nvim",
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    lazy = false
+  },
 }
 
 return plugins
