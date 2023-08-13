a6a0c4024f2c2ac743629a980efdbb168bf03ea5
Ben_Feldberg_Collins <b.feldberg.collins@accenture.com>
2023-08-13 11:41:21 +0100

    make paths relative

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 mkdir -p ~/.config/tmux
-cp -r tmux ~/.config
 
-cp -r nvim/* ~/.config/nvim/lua/custom/
+cp -r "$parent_path/tmux" ~/.config
+
+cp -r "$parent_path/nvim/*" ~/.config/nvim/lua/custom/
