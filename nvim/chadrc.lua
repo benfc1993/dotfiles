75c6b03f6f86ba0d06ec8776ef711e61697c3024
Ben_Feldberg_Collins <b.feldberg.collins@accenture.com>
2023-08-14 22:50:19 +0100

    add emoji status

--- nvim/chadrc.lua
+++ nvim/chadrc.lua
@@ -23,10 +23,12 @@ M.ui = {theme = 'chadracula',
           if not vim.b.gitsigns_head or vim.b.gitsigns_git_status then
             return ""
           end
+
           local git_status = vim.b.gitsigns_status_dict
-          local message =  (git_status.changed and git_status.changed ~= 0) and "\u{1F601}" or "no chage"
-          print(message)
-          return message
+          local changed =  (git_status.changed and git_status.changed ~= 0) and true or false
+          local added =  (git_status.added and git_status.added ~= 0) and true or false
+          local removed =  (git_status.changed and git_status.changed ~= 0) and true or false
+          return changed and added and removed and "\u{1F680}" or "\u{1F343}"
         end)()
       )
     end,
