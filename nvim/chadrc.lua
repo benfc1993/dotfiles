33327bc136b8e351ac9dfd1da6a14c101a82b96c
Ben_Feldberg_Collins <b.feldberg.collins@accenture.com>
2023-08-14 22:41:41 +0100

    add early return protection

--- nvim/chadrc.lua
+++ nvim/chadrc.lua
@@ -16,13 +16,16 @@ M.ui = {theme = 'chadracula',
       --   return ""
       -- end
 
-      local git_status = vim.b.gitsigns_status_dict
-      local message =  (git_status.changed  and "\u{1F601}" or "no chage")
-      print(message)
       table.insert(
         modules,
         2,
         (function()
+          if not vim.b.gitsigns_head or vim.b.gitsigns_git_status then
+            return ""
+          end
+          local git_status = vim.b.gitsigns_status_dict
+          local message =  (git_status.changed and git_status.changed ~= 0) and "\u{1F601}" or "no chage"
+          print(message)
           return message
         end)()
       )
