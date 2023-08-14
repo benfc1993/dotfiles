3ea964954f4003592403bec17470d6686f1b146a
Ben_Feldberg_Collins <b.feldberg.collins@accenture.com>
2023-08-14 23:23:48 +0100

    cleanup

--- nvim/chadrc.lua
+++ nvim/chadrc.lua
@@ -3,39 +3,11 @@ local M = {}
 
 M.ui = {theme = 'chadracula',
   hl_add = {
-    ["branchCleanHl"] = { fg = "green" },
-    ["branchModifiedHl"] = { fg = "red" }
-
+    ["branchCleanHl"] = { fg = "green", bg = "NONE", sp = "NONE" },
+    ["branchModifiedHl"] = { fg = "red", bg = "NONE", sp = "NONE" }
   },
   statusline = {
-    -- modules arg here is the default table of modules
     overriden_modules = function(modules)
-      -- modules[1] = (function()
-      --   return "MODE!"
-      -- end)()
-
-      -- adding a module between 2 modules
-      -- Use the table.insert functin to insert at specific index
-      -- This will insert a new module at index 2 and previous index 2 will become 3 now
-      -- if not vim.b.gitsigns_head or vim.b.gitsigns_git_status then
-      --   return ""
-      -- end
-
-      -- table.insert(
-      --   modules,
-      --   2,
-      --   (function()
-      --     if not vim.b.gitsigns_head or vim.b.gitsigns_git_status then
-      --       return ""
-      --     end
-      --
-      --     local git_status = vim.b.gitsigns_status_dict
-      --     local changed =  (git_status.changed and git_status.changed ~= 0) and true or false
-      --     local added =  (git_status.added and git_status.added ~= 0) and true or false
-      --     local removed =  (git_status.changed and git_status.changed ~= 0) and true or false
-      --     return changed and added and removed and "\u{1F680}" or "\u{1F343}"
-      --   end)()
-      -- )
       modules[3] = (function()
         if not vim.b.gitsigns_head or vim.b.gitsigns_git_status then
           return ""
