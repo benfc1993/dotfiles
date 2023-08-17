4de0846ffdfae2128b289a176f6a3aad91973a60
Ben <benfeldbergcollins@hotmail.co.uk>
2023-08-17 17:23:15 +0100

    swap to vanilla neovim

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
+rm -rf ~/.config/nvim
+
 mkdir -p ~/.config/tmux
 
 ln -s "$parent_path/tmux/tmux.conf" ~/.config/tmux/tmux.conf
@@ -9,7 +11,7 @@ ln -s "$parent_path/tmux/tmux.conf" ~/.config/tmux/tmux.conf
 chmod a+u "$parent_path/tmux/plugins/fileSearch" 
 ln -s "$parent_path/tmux/plugins/fileSearch" /usr/local/bin/fileSearch 
 
-cd ~/.config/nvim/lua && ln -s "$parent_path/nvim" custom
+cd "$parent_path/nvim" && ln -s $(pwd) ~/.config/nvim
 
 ln -s "$parent_path/vimcd" /usr/local/bin/vimcd
 
