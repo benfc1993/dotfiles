1fe3ba30e79cdfc4de8f4e7fb699940ba67734b1
Ben <benfeldbergcollins@hotmail.co.uk>
2023-08-18 14:34:30 +0100

    add auto source and format

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -8,5 +8,3 @@ vim.keymap.set("n", "<C-p>", function() ui.nav_file(1) end)
 vim.keymap.set("n", "<C-o>", function() ui.nav_file(2) end)
 vim.keymap.set("n", "<C-i>", function() ui.nav_file(3) end)
 vim.keymap.set("n", "<C-u>", function() ui.nav_file(4) end)
-
-
--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,19 @@
 require("custom")
+
+vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
+    pattern = "*",
+    callback = function()
+        vim.lsp.buf.format()
+    end
+})
+vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
+    pattern = "*/nvim/*.lua",
+    callback = function()
+        local fp = vim.fn.expand('%:p')
+        if string.find(fp, "nvim/init.lua") ~= nil then return end
+        vim.schedule(function()
+            print("Testing")
+            vim.cmd("so")
+        end)
+    end
+})
