ead5ecc9e292ea23fc1018ab37bdfc8f414c1be8
Ben_Feldberg_Collins <b.feldberg.collins@accenture.com>
2023-08-19 11:34:40 +0100

    fix folder structure

--- nvim/lua/custom/after/plugin/init.lua
+++
@@ -1,8 +0,0 @@
-require("custom.after.plugin.colors")
-require("custom.after.plugin.lsp")
-require("custom.after.plugin.lazygit")
-require("custom.after.plugin.lualine")
-require("custom.after.plugin.harpoon")
-require("custom.after.plugin.telescope")
-require("custom.after.plugin.toggleterm")
-require("custom.after.plugin.treesitter")
