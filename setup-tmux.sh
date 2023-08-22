4c351c49f9d388c49bd87fd4ff7995332841b398
Ben <benfeldbergcollins@hotmail.co.uk>
2023-08-22 19:26:55 +0100

    add tmux cheatsheet

--- setup-tmux.sh
+++ setup-tmux.sh
@@ -2,7 +2,25 @@
 
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
-rm -rf ~/.config/tmux/tmux.conf
+if [[ ! $(dpkg-query -l glow)  ]]; then
+    echo "installing dependancies"
+    sudo mkdir -p /etc/apt/keyrings
+    curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
+    echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
+    sudo apt update && sudo apt install glow
+fi
+
+if [[ ! $(dpkg-query -l tmux) ]]; then
+    sudo apt update;
+    sudo apt install tmux
+fi
+
+if [[ ! -d ~/.tmux/plugins/tpm ]]; then
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
+
+rm -rf ~/.config/tmux
 sudo rm -rf /usr/local/bin/fileSearch
 sudo rm -rf /usr/local/bin/vimcd
 
@@ -13,5 +31,13 @@ ln -s "$parent_path/tmux/tmux.conf" ~/.config/tmux/tmux.conf
 chmod a+u "$parent_path/tmux/plugins/fileSearch" 
 sudo ln -s "$parent_path/tmux/plugins/fileSearch" /usr/local/bin/fileSearch 
 
-chmod a+u "$parent_path/vimcd" 
-sudo ln -s "$parent_path/vimcd" /usr/local/bin/vimcd
+chmod a+u "$parent_path/vimcd.sh" 
+sudo ln -s "$parent_path/vimcd.sh" /usr/local/bin/vimcd
+
+ln -s "$parent_path/tmux/cheatsheet.md" ~/.config/tmux/cheatsheet.md
+ln -s "$parent_path/tmux/cheatsheet.sh" ~/.config/tmux/cheatsheet
+
+tmux source ~/.config/tmux/tmux.conf
+
+
+./.tmux/plugins/tpm/bindings/install_plugins
---
+++ tmux/cheatsheet.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+glow ~/.config/tmux/cheatsheet.md --pager
