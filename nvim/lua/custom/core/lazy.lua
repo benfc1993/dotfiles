6bbdc513b4e3e0d7481a9fd36545ab1fb23abdf4
Ben <benfeldbergcollins@hotmail.co.uk>
2023-11-23 16:02:13 +0000

    swap to lazy

---
+++ nvim/lua/custom/core/lazy.lua
@@ -0,0 +1,100 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup({
+    -- Packer can manage itself
+    {
+        "startup-nvim/startup.nvim",
+        requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
+    },
+    {
+        'christoomey/vim-tmux-navigator',
+        config = function()
+            vim.g.tmux_navigator_no_mappings = 1
+        end
+    },
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.2',
+        -- or                            , branch = '0.1.x',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    'nvim-telescope/telescope-symbols.nvim',
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+    },
+    {
+        { 'nvim-treesitter/nvim-treesitter' },
+        'theprimeagen/harpoon',
+        'mbbill/undotree',
+        {
+            "kdheepak/lazygit.nvim",
+            -- optional for floating window border decoration
+            requires = {
+                "nvim-lua/plenary.nvim",
+            },
+        },
+        'rust-lang/rust.vim',
+        ft = 'rust',
+        init = function()
+            print('testing')
+            vim.g.rustfmt_autosave = 1
+        end
+    },
+    {
+        'neovim/nvim-lspconfig',
+        dependencies = {
+            'williamboman/mason.nvim',           -- Optional
+            'williamboman/mason-lspconfig.nvim', -- Optional
+
+            -- Autocompletion
+            'folke/neodev.nvim',
+            'hrsh7th/nvim-cmp',     -- Required
+            'hrsh7th/cmp-nvim-lsp', -- Required
+            -- { 'hrsh7th/cmp-nvim-lua' },
+            'hrsh7th/cmp-path',
+            'hrsh7th/cmp-buffer',
+            'saadparwaiz1/cmp_luasnip',
+            'L3MON4D3/LuaSnip', -- Required
+
+        }
+    },
+    'jose-elias-alvarez/null-ls.nvim',
+    'MunifTanjim/prettier.nvim',
+    'simrat39/rust-tools.nvim',
+    "akinsho/toggleterm.nvim",
+    {
+        'numToStr/Comment.nvim',
+        config = function()
+            require('Comment').setup()
+        end
+    },
+    { 'nvim-tree/nvim-web-devicons' },
+    'nvim-tree/nvim-tree.lua',
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' }
+    },
+    'tpope/vim-obsession',
+    -- Themes
+    {
+        'Mofiqul/dracula.nvim',
+        as = 'dracula',
+    },
+    "bluz71/vim-nightfly-colors",
+    { "catppuccin/nvim",            as = "catppuccin" },
+    "ellisonleao/gruvbox.nvim",
+
+})
