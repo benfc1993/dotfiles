eaf6f7ae43e007f45725785150f517a6fb067d33
Ben <benfeldbergcollins@hotmail.co.uk>
2023-11-23 23:56:04 +0000

    add urls to google search

--- tmux/plugins/google-search.sh
+++ tmux/plugins/google-search.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-read -p 'Search >' search;
+read -p 'search | url >' search;
 
-search="${search// /+}"
+if [[ "$search" =~ ^[a-zA-Z0-9\/]*\.[a-zA-Z0-9\/]*$ ]]; then
+   python3 -m webbrowser -t "https://$search"    
+else
 
-python3 -m webbrowser -t "https://google.com/search?q=$search";
+    search="${search// /+}"
+
+    python3 -m webbrowser -t "https://google.com/search?q=$search";
+fi
 
 exit 0;
 
