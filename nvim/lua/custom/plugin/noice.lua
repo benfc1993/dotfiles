c234c8e3c77442adbf4c70044c128b82256b57e2
Ben <benfeldbergcollins@hotmail.co.uk>
2023-11-27 00:20:18 +0000

    status bar and notify changes

--- nvim/lua/custom/plugin/noice.lua
+++ nvim/lua/custom/plugin/noice.lua
@@ -1,5 +1,7 @@
 require("notify").setup({
     background_colour = "#000000",
+    -- level = vim.log.levels.WARN,
+    render = 'minimal'
 })
 
 require("noice").setup({
--- nvim/lua/custom/plugin/prettier.lua
+++ nvim/lua/custom/plugin/prettier.lua
@@ -20,7 +20,7 @@ prettier.setup({
         condition = function()
             return prettier.config_exists({
                 -- if `false`, skips checking `package.json` for `"prettier"` key
-                check_package_json = true,
+                check_package_json = false,
             })
         end,
         runtime_condition = function(params)
