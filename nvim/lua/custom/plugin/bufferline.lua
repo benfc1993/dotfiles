d76de9ac21c83d9f6b0f7d4dda3b13d612e34964
ben feldberg collins <ben.feldbergcollins@sainsburys.co.uk>
2023-12-12 14:27:04 +0000

    fix close all mapping

--- nvim/lua/custom/plugin/bufferline.lua
+++ nvim/lua/custom/plugin/bufferline.lua
@@ -6,7 +6,7 @@ end, '[bufferline] cycle forward')
 nmap('<leader><S-Tab>', function()
     bufferline.cycle(-1)
 end, '[bufferline] cycle forward')
-nmap('<C-W>', bufferline.close_others, '[Bufferline] close others')
+nmap('<leader>kw', bufferline.close_others, '[Bufferline] close others')
 for i = 1, 9, 1 do
     nmap('<leader>' .. i, function()
             bufferline.go_to(i)
