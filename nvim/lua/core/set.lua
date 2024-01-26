73520682f71ddf7c402cb517312ef57f202df611
ben feldberg collins <ben.feldbergcollins@sainsburys.co.uk>
2024-01-26 12:40:49 +0000

    "$1"

--- nvim/lua/core/set.lua
+++ nvim/lua/core/set.lua
@@ -1,8 +1,6 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
--- vim.opt.cmdheight = 0
-
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -20,9 +18,9 @@ vim.opt.termguicolors = true -- sets color matching for better themes
 
 vim.opt.scrolloff = 8 -- scroll offset before moving page
 
-vim.opt.spelllang = "en_gb"
+vim.opt.spelllang = "en_us"
 vim.opt.spellsuggest = "best,5"
-vim.opt.spelloptions = "camel"
+-- vim.opt.spelloptions = "camel"
 vim.opt.spell = true
 
 vim.opt.cursorline = true
